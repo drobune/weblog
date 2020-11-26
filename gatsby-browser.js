0b92a9e433e3a734e1edcd6ba4c244a153a6a8c3
drobune <yuichi.sano@drobune.nl>
2020-11-26 16:27:31 +0900

    remove trash file

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-
-export const onRouteUpdate = ({ location, prevLocation }) => {
-  console.log("new pathname", location.pathname)
-  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
-}
-
